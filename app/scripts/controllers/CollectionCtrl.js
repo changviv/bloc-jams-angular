324ebeb446e20e1b1878160260c426e82069493a
Vivien <vchang103@gmail.com>
2016-10-12 23:05:55 -0700

    completed Assignment -6

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,12 +1,9 @@
 (function() {
      function CollectionCtrl(Fixtures) {
-         this.albums = [];
-         for (var i=0; i < 12; i++) {
-            this.albums.push(Fixtures.getAlbum);
-         }
+        this.albums = Fixtures.getCollection(12);
       }
  
      angular
          .module('blocJams')
-         .controller('CollectionCtrl', CollectionCtrl);
+         .controller('CollectionCtrl', ['Fixtures', CollectionCtrl]);
  })();
